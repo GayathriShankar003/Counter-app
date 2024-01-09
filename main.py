78b1e7fb859450d13207c4fa5f92434b8da7f5b5
GAYATHRIS30 <25038877-GAYATHRIS30@users.noreply.replit.com>
2024-01-09 13:33:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from replit import db
+
+app = Flask(__name__)
+
+if 'number' not in db:
+  db['number'] = 0
+
+@app.route('/')
+def home():
+  return render_template('index.html', number=db['number'])
+
+@app.route('/increment')
+def increment():
+  db['number'] += 1
+  return render_template('index.html', number=db['number']) 
+
+@app.route('/decrement')
+def decrement():
+  db['number'] -= 1
+  return render_template('index.html', number=db['number']) 
+
+
+app.run(host='0.0.0.0', port=81)
